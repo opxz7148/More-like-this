6c0e9de6c3f06c34adec332d9c344fa630d57fbe
Pasu Sangiemsin <opxz7148@gmail.com>
2024-05-03 15:03:31 +0700

    Implement add artist method

--- artist_db.py
+++ artist_db.py
@@ -8,7 +8,8 @@ import dotenv
 import pandas as pd
 from datetime import date
 
-class  Artist_db():
+
+class ArtistDb:
     """
     Class for working with artist discography data csv file by utilizing pandas dataframe and spotipy library
     """
@@ -50,10 +51,6 @@ class  Artist_db():
                 'popularity',
                 'img_url',
                 'external_url',
-                'num_album',
-                'num_single',
-                'average_toptrack_pop',
-                'average_toptrack_duration'
             ]
         )
 
@@ -69,10 +66,6 @@ class  Artist_db():
             {
                 'followers': 'int16',
                 'popularity': 'int8',
-                'num_album': 'int16',
-                'num_single': 'int16',
-                'average_toptrack_pop': 'float32',
-                'average_toptrack_duration': 'float32'
             },
             copy=True
         )
@@ -88,7 +81,6 @@ class  Artist_db():
                 'release_date_precision',
                 'total_tracks',
                 'type',
-                'album_duration_ms',
                 'popularity'
             ]
         )
@@ -103,13 +95,13 @@ class  Artist_db():
         # Set datatypes for each column
         self._album = self._album.astype(
             {
-                'release_date': 'datetime64[ns]',
                 'total_tracks': 'int16',
-                'album_duration_ms': 'int64',
                 'popularity': 'int8'
             },
             copy=True
         )
+
+        self._album['release_date'].astype('datetime64[ns]')
 
         track_correct_column = np.array(
             [
@@ -146,7 +138,115 @@ class  Artist_db():
             type='artist'
         )['artists']['items']
 
-        return [(artist['name'], artist['genres']) for artist in result]
+        return [(artist['name'], artist['genres'], artist['id']) for artist in result]
+
+    def add_artist(self, artist_id):
+
+        if artist_id in self._artist['artist_id'].values:
+            return
+
+        artist_detail = self._sp.artist(artist_id)
+
+        try:
+            img_url = artist_detail['images'][0]['url']
+        except IndexError:
+            img_url = None
+
+        self._artist.loc[len(self._artist)] = {
+            'artist_name': artist_detail['name'],
+            'artist_id': artist_detail['id'],
+            'genres': artist_detail['genres'],
+            'followers': artist_detail['followers']['total'],
+            'popularity': artist_detail['popularity'],
+            'img_url': img_url,
+            'external_url': artist_detail['external_urls']['spotify'],
+        }
+        artist_album = self._sp.artist_albums(artist_id, album_type=('album', 'single'))['items']
+        album_list = [album['id'] for album in artist_album]
+        self.__add_album(album_list)
+
+    def __add_album(self, album_list):
+
+        all_album = self._sp.albums(album_list)['albums']
+
+        track_list = []
+
+        for album_detail in all_album:
+
+            try:
+                img_url = album_detail['images'][0]['url']
+            except IndexError:
+                img_url = None
+
+            release_date = np.datetime64(album_detail['release_date'], "D")
+
+            self._album.loc[len(self._album)] = {
+                'artist_id': album_detail['artists'][0]['id'],
+                'external_url': album_detail['external_urls']['spotify'],
+                'img_url': img_url,
+                'album_name': album_detail['name'],
+                'album_id': album_detail['id'],
+                'release_date': release_date,
+                'release_date_precision': 'day',
+                'total_tracks': album_detail['total_tracks'],
+                'type': album_detail['type'],
+                'popularity': album_detail['popularity']
+            }
+
+            track_list += [track['id'] for track in album_detail['tracks']['items']]
+
+        self.__add_track(track_list)
+
+    def __add_track(self, track_list):
+
+        print(track_list)
+
+        def add_track(track_id_list):
+
+            all_track = self._sp.tracks(track_id_list)['tracks']
+
+            for track_detail in all_track:
+
+                self._track.loc[len(self._track)] = {
+                    'artist_id': track_detail['artists'][0]['id'],
+                    'album_id': track_detail['album']['id'],
+                    'track_id': track_detail['id'],
+                    'track_name': track_detail['name'],
+                    'popularity': track_detail['popularity'],
+                    'duration_ms': track_detail['duration_ms']
+                }
+
+        track_id = track_list
+
+        limit = 50
+
+        while len(track_id) > limit:
+
+            add_track(track_id[0:limit])
+            track_id = track_id[limit:]
+
+        add_track(track_id)
+
+    def update_csv(self):
+        self._artist.to_csv('csv/artist.csv', index=False)
+        self._album.to_csv('csv/album.csv', index=False)
+        self._track.to_csv('csv/track.csv', index=False)
 
 
+class SelectedArtist:
+    """
+    Class contain neccessary method and attribute for displaying selected artist detail
+    """
+
+    def __init__(
+            self,
+            artist: pd.DataFrame,
+            album: pd.DataFrame,
+            track: pd.DataFrame
+    ):
+        """
+        :param artist: Dataframe contain selected artist detail
+        :param album: Dataframe contain all selected artist album
+        :param track: Dataframe contain all track made by selected artist
+        """
 
